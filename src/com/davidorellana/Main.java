f4ad65eab922e3885de2c40e5aad6e9df3a3645c
David Orellana <davidorellana_98@hotmail.com>
2022-10-24 22:09:32 -0500

    ShopProductI interface modification and addition of new methods

--- src/com/davidorellana/Main.java
+++ src/com/davidorellana/Main.java
@@ -8,36 +8,45 @@ import com.davidorellana.product.ProductInformation;
 public class Main {
     public static void main(String[] args) {
         System.out.println("***** Product 0 *****");
-        PackagingDescription packagingDescriptionOne = new PackagingDescription("Bolsa", 11000.0, "ml");
-        Category categoryOne = new Category("Lacteo/Huevos", true);
-        ProductInformation productInformationOne = new ProductInformation(0L,"Leche Entera Apina", packagingDescriptionOne, categoryOne, "Leche", 3100.0, "https://www.eurosupermercados.com/eurosupermercado2020//contenidos/images/ecommerce_productos/7702001041404.jpg");
+        PackagingDescription packagingDescription = new PackagingDescription("Bolsa", 11000.0, "ml");
+        Category category = new Category("Lacteo/Huevos", true);
+        ProductInformation productInformation = new ProductInformation(0L,"Leche Entera Apina", packagingDescription, category, "Leche", 3100.0, "https://www.eurosupermercados.com/eurosupermercado2020//contenidos/images/ecommerce_productos/7702001041404.jpg");
         StoreOwner storeOwner = new StoreOwner("Camila", "Ordoñez",35);
-        String isCreatedProduct = storeOwner.createNewProduct(productInformationOne) ? "created" : "not created";
-        System.out.println("Product " + productInformationOne.getId() + " " + isCreatedProduct);
-        System.out.println("Product search by id and name: " + storeOwner.findProductByNameAndId(0L, "Leche Entera Apina"));
-        System.out.println("Product update: " + storeOwner.updateProductInformation(productInformationOne.getId(), new ProductInformation(0L,"Leche Alpina Entera", packagingDescriptionOne, categoryOne, "Leche POLVO", 3100.0, "https://www.eurosupermercados.com/eurosupermercado2020//contenidos/images/ecommerce_productos/7702001041404.jpg")));
-
+        String isCreatedProduct = storeOwner.addProduct(productInformation) ? "created." : "not created.";
+        System.out.println("Product " + productInformation.getId() + " " + isCreatedProduct);
+        System.out.println("Product search by id: " + storeOwner.findProductById(0L));
+        System.out.println("Product search by name: " + storeOwner.findProductByName("Leche Entera Apina"));
+        ProductInformation newProduct =  storeOwner.updateProductInformation(new ProductInformation(0L,"Leche Alpina Entera", packagingDescription, category, "Leche POLVO", 3100.0, "https://www.eurosupermercados.com/eurosupermercado2020//contenidos/images/ecommerce_productos/7702001041404.jpg"));
+        System.out.println("Product update: " + newProduct);
+        System.out.println("Product search by id: " + storeOwner.findProductById(0L));
 
         System.out.println("***** Product 1 *****");
-        PackagingDescription packagingDescriptionTwo = new PackagingDescription("Caja", 1000.0, "ml");
-        Category categoryTwo = new Category("Vinos y Licores", false);
-        ProductInformation productInformationTwo = new ProductInformation(1L,"Aguardiente Antiqueño Verde", packagingDescriptionTwo, categoryTwo, "Aguardeinte", 45600.0, "https://metrocolombiafood.vteximg.com.br/arquivos/ids/259143-1000-1000/7702049001675.jpg?v=637172016121530000");
-        isCreatedProduct = storeOwner.createNewProduct(productInformationTwo) ? "created" : "not created";
-        System.out.println("Product " + productInformationTwo.getId() + " " + isCreatedProduct);
-        System.out.println("Product search by id and name: " + storeOwner.findProductByNameAndId(1L, "Aguardiente Antiqueño"));
-        System.out.println("Product update: " + storeOwner.updateProductInformation(productInformationTwo.getId(), new ProductInformation(1L,"Vodka Smirnoff", packagingDescriptionTwo, categoryTwo, "Vodka", 3100.0, "https://www.eurosupermercados.com/eurosupermercado2020//contenidos/images/ecommerce_productos/7702001041404.jpg")));
-
+        PackagingDescription packagingDescriptionOne = new PackagingDescription("Caja", 1000.0, "ml");
+        Category categoryOne = new Category("Vinos y Licores", false);
+        ProductInformation productInformationOne = new ProductInformation(1L,"Aguardiente Antiqueño Verde", packagingDescriptionOne, categoryOne, "Aguardeinte", 45600.0, "https://metrocolombiafood.vteximg.com.br/arquivos/ids/259143-1000-1000/7702049001675.jpg?v=637172016121530000");
+        isCreatedProduct = storeOwner.addProduct(productInformationOne) ? "created." : "not created.";
+        System.out.println("Product " + productInformationOne.getId() + " " + isCreatedProduct);
+        System.out.println("Product search by id: " + storeOwner.findProductById(1L));
+        System.out.println("Product search by name: " + storeOwner.findProductByName("Aguardiente Antiqueño Verde"));
 
         System.out.println("***** Product 2 *****");
-        PackagingDescription packagingDescriptionThree = new PackagingDescription("Bolsa", 750.0, "ml");
-        Category categoryThree = new Category("Vinos y Licores", false);
-        ProductInformation productInformationThree = new ProductInformation(2L,"Whiskey Jack Daniels Honey", packagingDescriptionThree, categoryThree, "Vodka", 107700.0, "https://jumbocolombiafood.vteximg.com.br/arquivos/ids/179058-1000-1000/Jack-Honey-750.jpg?v=636124971865400000");
-        isCreatedProduct = storeOwner.createNewProduct(productInformationThree) ? "created" : "not created";
-        System.out.println("Product " + productInformationThree.getId() + " " + isCreatedProduct);
-        System.out.println("Product search by id and name: " + storeOwner.findProductByNameAndId(2L, "Whiskey Jack Daniels Honey"));
+        PackagingDescription packagingDescriptionTwo = new PackagingDescription("Bolsa", 750.0, "ml");
+        Category categoryTwo = new Category("Vinos y Licores", false);
+        ProductInformation productInformationTwo = new ProductInformation(2L,"Whiskey Jack Daniels Honey", packagingDescriptionTwo, categoryTwo, "Vodka", 107700.0, "https://jumbocolombiafood.vteximg.com.br/arquivos/ids/179058-1000-1000/Jack-Honey-750.jpg?v=636124971865400000");
+        isCreatedProduct = storeOwner.addProduct(productInformationTwo) ? "created." : "not created.";
+        System.out.println("Product " + productInformationTwo.getId() + " " + isCreatedProduct);
+        System.out.println("Product search by id: " + storeOwner.findProductById(2L));
+        System.out.println("Product search by name: " + storeOwner.findProductByName("Whiskey Jack Daniels Honey"));
+        System.out.println("Product update: " + storeOwner.updateProductInformation(new ProductInformation(2L,"Vodka Smirnoff", packagingDescriptionTwo, categoryTwo, "Vodka", 3100.0, "https://www.eurosupermercados.com/eurosupermercado2020//contenidos/images/ecommerce_productos/7702001041404.jpg")));
+        System.out.println("Product search by name: " + storeOwner.findProductByName("Vodka Smirnoff"));
 
-        System.out.println("***** Product query *****");
-        System.out.println("Product query by category: " + storeOwner.queryProductWithSameCategory("Lacteo/Huevos"));
+        System.out.println("***** Find and get products *****");
+        System.out.println("Products with category: " + storeOwner.queryProductWithSameCategory("Lacteo/Huevos"));
+        System.out.println("List of all products: " + storeOwner.getAllProducts());
 
+        System.out.println("***** Removed products *****");
+        System.out.println(storeOwner.deleteProduct(newProduct));
+        System.out.println(storeOwner.deleteProduct(productInformationOne));
+        System.out.println(storeOwner.deleteProduct(productInformationTwo));
     }
 }
